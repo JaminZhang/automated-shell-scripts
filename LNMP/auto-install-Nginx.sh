af244359384df07ffbae836e61b559d36901b8f0
JaminZhang <zhangjamin@163.com>
2014-10-15 18:31:30 +0800

    Update auto-install-Nginx.sh
    Add pcre zlib openssl installation

--- LNMP/auto-install-Nginx.sh
+++ LNMP/auto-install-Nginx.sh
@@ -6,11 +6,13 @@
 
 SOFT_DIR="/home/jamin/soft"
 PCRE_VERSION="8.35"
+ZLIB_VERSION="1.2.8"
+OPENSSL_VERSION="1.0.1i"
 NGINX_VERSION="1.4.7"
 NGINX_INSTALL_DIR="/app/nginx-$NGINX_VERSION"
 NGINX_LN_DIR="/app/nginx"
 
-echo "-----Step 01: Install pcre-----"
+echo "-----Step 01: Install pcre zlib openssl-----"
 [ ! -d "$SOFT_DIR" ] && mkdir $SOFT_DIR
 cd $SOFT_DIR
 [ ! -f pcre-$PCRE_VERSION.tar.gz ] && \
@@ -21,6 +23,28 @@ tar xzf pcre-$PCRE_VERSION.tar.gz
 cd pcre-$PCRE_VERSION
 ./configure
 make && make install
+cd ../
+
+[ ! -f zlib-$ZLIB_VERSION.tar.gz ] && \
+wget http://zlib.net/zlib-$ZLIB_VERSION.tar.gz
+[ ! -f zlib-$ZLIB_VERSION.tar.gz ] && exit 1
+
+tar xzf zlib-$ZLIB_VERSION.tar.gz
+cd zlib-$ZLIB_VERSION
+./configure
+make && make install
+cd ../
+
+[ ! -f openssl-$OPENSSL_VERSION.tar.gz ] && \
+wget http://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+[ ! -f openssl-$OPENSSL_VERSION.tar.gz ] && exit 1
+
+tar xzf openssl-$OPENSSL_VERSION.tar.gz
+cd openssl-$OPENSSL_VERSION
+./config
+make && make install
+cd ../
+
 
 echo "-----Step 02: Add nginx user-----"
 useradd -s /sbin/nologin -M nginx
@@ -39,8 +63,13 @@ cd nginx-${NGINX_VERSION}
 --prefix=$NGINX_INSTALL_DIR \
 --user=nginx \
 --group=nginx \
+--with-http_ssl_module \
 --with-http_stub_status_module \
---with-http_ssl_module
+--with-pcre=${SOFT_DIR}/pcre-$PCRE_VERSION \
+--with-zlib=${SOFT_DIR}/zlib-$ZLIB_VERSION \
+--with-openssl=${SOFT_DIR}/openssl-$OPENSSL_VERSION
+
+
 
 make && make install
 
