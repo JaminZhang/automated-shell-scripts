33c86ff99314ca691d033674dbc9285aa7e83cdf
JaminZhang <zhangjamin@163.com>
2015-09-17 13:58:16 +0800

    Create deploy-ssserver.sh

---
+++ ShadowSocks/deploy-ssserver.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+E_WRONG_PARAM=65
+E_EXISTED=66
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: `basename $0` server_port password"
+	exit $E_WRONG_PARAM
+fi
+
+if [ -e /usr/bin/ssserver ]
+then
+	echo "/usr/bin/ssserver exists, now exit."
+	exit $E_EXISTED
+fi
+
+yum install python-pip -y
+
+pip install shadowsocks
+
+cat > /etc/shadowsocks.json << EOF
+{
+    "server":"0.0.0.0",
+    "server_port":$server_port,
+    "local_address": "127.0.0.1",
+    "local_port":1081,
+    "password":"$password",
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open": false,
+    "workers": 1
+}
+EOF
+
+/usr/bin/ssserver -c /etc/shadowsocks.json -d start
+
+echo "#Start ssserver" >> /etc/rc.local
+echo "/usr/bin/ssserver -c /etc/shadowsocks.json -d start" >> /etc/rc.local
